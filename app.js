f547de3747ae2bbd74a7ed119a35cc2920c8d395
Katrina Velez <katrinamvelez@gmail.com>
2016-04-27 17:47:16 -0400

    twitter clone app

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var http = require('http');
+var app = express(); // creates an instance of an express application
+var server = http.createServer();
+
+app.use(function (req, res, next) {
+	// do your logging here
+	console.log(req.method, req.url, res.statusCode);
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    next();
+});
+
+app.use('/special/*', function(req, res, next){
+	console.log('you have reached the special area');
+});
+
+app.get('/special/', function(req, res){
+	res.send('you have reached the special area');
+});
+
+app.get('/', function (req, res) {
+  res.send('Welcome!');
+});
+
+app.get('/news', function (req, res) {
+  res.send('This is the news!');
+});
+
+
+
+app.listen(3000, function(){
+	console.log("Server listening on port 3000");
+})
